a92450cbe4186554ce5ddf84bcbcb882302732e9
sunnyssarai98 <sunnyssarai98@gmail.com>
2021-11-10 17:13:15 -0600

    Remove demo code; Add screens

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:drink_deals/screens/authenticate/authenticate.dart';
+import 'package:drink_deals/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
