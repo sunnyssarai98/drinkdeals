93cb0389c846f4a7c693f3f6a0db933de56c25c4
sunnyssarai98 <sunnyssarai98@gmail.com>
2021-11-10 19:20:26 -0600

    Create custom user object

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,15 +1,20 @@
+import 'package:drink_deals/models/myuser.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  //anon
+  // Creating user object based on FirebaseUser
+  MyUser? _userFromFirebaseUser(User user) {
+    return user != null ? MyUser(uid: user.uid) : null;
+  }
 
+  //anon
   Future signInAnon() async {
     try {
       UserCredential result = await _auth.signInAnonymously();
       User? user = result.user;
-      return user;
+      return _userFromFirebaseUser(user!);
     } catch (e) {
       print(e.toString());
       return null;
