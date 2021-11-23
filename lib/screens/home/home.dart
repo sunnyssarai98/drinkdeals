852e131b4b66228324a03973d1beee0261489bf5
ryancourson <rcours1@lsu.edu>
2021-11-23 02:54:36 -0600

    Profile Page Added
    Addition of Profile page, displays favorite deals which is for now, hardcoded.

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:drink_deals/screens/home/deal.dart';
 import 'package:drink_deals/services/auth.dart';
 import 'package:flutter/material.dart';
 
@@ -8,15 +9,137 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
+late List<Deal> deals = [
+  Deal(barName: 'Freds', deal: '\$5 Drinks', picURL: 'freds.png'),
+  Deal(barName: 'Mikes', deal: 'Free Drinks', picURL: 'mikes.png'),
+  Deal(barName: 'Reggies', deal: '\$1 Shots', picURL: 'Reggies')
+];
+
 class _HomeState extends State<Home> {
+  void setupDeals() async {
+    deals = [
+      Deal(barName: 'Freds', deal: '\$5 Drinks', picURL: 'freds.png'),
+      Deal(barName: 'Mikes', deal: 'Free Drinks', picURL: 'mikes.png'),
+      Deal(barName: 'Reggies', deal: '\$1 Shots', picURL: 'Reggies')
+    ];
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupDeals();
+  }
+
   final AuthService _auth = AuthService();
 
   int _currentIndex = 0;
 
+  // Deal d1 = deals[1];
   var tabs = [
     Center(child: Text('Home')),
     Center(child: Text('Map')),
-    Center(child: Text('Account')),
+    Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('Profile'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/user.png'),
+                radius: 40.0,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Center(
+              child: Text(
+                'LOUISIANA STATE UNIVERSITY',
+                style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                ),
+              ),
+            ),
+            Divider(
+              height: 50.0,
+              color: Colors.grey[700],
+            ),
+
+            Text(
+              'USERNAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'drinkdeals_user1',
+              style: TextStyle(
+                  color: Colors.amber[200],
+                  letterSpacing: 2.0,
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            // ignore: prefer_const_constructors
+            Text(
+              'FAVORITE DEALS',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Card(
+                child: ListTile(
+              onTap: () {},
+              title: Text(
+                deals[0].barName + '\t - \t' + deals[0].deal,
+              ),
+              trailing: InkWell(
+                  child: CircleAvatar(
+                backgroundImage: AssetImage('assets/freds.PNG'),
+              )),
+            )),
+            Card(
+                child: ListTile(
+              onTap: () {},
+              title: Text(deals[1].barName + '\t - \t' + deals[1].deal),
+              trailing: InkWell(
+                  child: CircleAvatar(
+                backgroundImage: AssetImage('assets/mikes.PNG'),
+              )),
+            )),
+            Card(
+              child: ListTile(
+                onTap: () {},
+                title: Text(deals[2].barName + '\t - \t' + deals[2].deal),
+                trailing: InkWell(
+                    child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/reggies.PNG'),
+                )),
+              ),
+            ),
+          ],
+        ),
+      ),
+    )
   ];
 
   @override
