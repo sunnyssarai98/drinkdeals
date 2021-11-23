2a6b0a6fbfe0c7c77bab2f1a77a8c32d8783234d
sunnyssarai98 <sunnyssarai98@gmail.com>
2021-11-23 12:36:22 -0600

    Add Button to Map

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -220,12 +220,30 @@ class _MapScreenState extends State<MapScreen> {
     zoom: 13,
   );
 
+  late GoogleMapController _googleMapController;
+
+  @override
+  void dispose() {
+    _googleMapController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: GoogleMap(
-            myLocationButtonEnabled: false,
-            zoomControlsEnabled: false,
-            initialCameraPosition: _initialCameraPosition));
+      body: GoogleMap(
+        myLocationButtonEnabled: false,
+        zoomControlsEnabled: false,
+        initialCameraPosition: _initialCameraPosition,
+        onMapCreated: (controller) => _googleMapController = controller,
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).primaryColor,
+        foregroundColor: Colors.black,
+        onPressed: () => _googleMapController.animateCamera(
+            CameraUpdate.newCameraPosition(_initialCameraPosition)),
+        child: const Icon(Icons.center_focus_strong),
+      ),
+    );
   }
 }
